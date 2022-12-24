b5f0b1f1ef026172e418ecab8c9cc53e685437e7
Farjana Farjo <121154389+farjanafarjo10@users.noreply.github.com>
2022-12-24 21:00:58 +0600

    Add files via upload

---
+++ Systematic/js/main.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function () {
+    jQuery('.slider').bxSlider();
+	jQuery('#main_menu').meanmenu({
+        meanMenuContainer:'#mobile_menu',
+        meanScreenWidht: 925,
+    });
+});
+
